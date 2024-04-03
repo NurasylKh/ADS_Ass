b859f924bdf79a847d8611213101a103f9354ebe
ASUS <231001@astanait.edu.kz>
2024-04-03 15:52:31 +0500

    Nurasyl Khurmet ADS Assignment_1

---
+++ src/main/java/kz/aitu/ads_ass/AdsAssApplication.java
@@ -0,0 +1,13 @@
+package kz.aitu.ads_ass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsAssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsAssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/aitu/ads_ass/AdsAssApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aitu.ads_ass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsAssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
